bbdb75b4108a2d2ddbcc0703a2d8e251527a73ce
zzzorgo <selyanin.dmitriy@gmail.com>
2026-01-24 13:52:24 +0100

    moved battery logic to a separate file

---
+++ src/battery.h
@@ -0,0 +1,3 @@
+#pragma once
+
+float read_battery_voltage();
--- src/main.c
+++ src/main.c
@@ -11,46 +11,7 @@
 #include <zephyr/drivers/sensor/scd4x.h>
 
 #include "bluetoothExposure.h"
-
-float read_battery_voltage() {
-  // The volatile keyword is a type qualifier in C/C++ that tells the compiler a variable's value might change in ways that the compiler cannot detect from the code alone. 
-  // Essentially, it says: "Don't optimize access to this variable because its value might change unexpectedly."
-  volatile uint32_t raw_value = 0;
-  // Configure SAADC
-  NRF_SAADC->ENABLE = 1;
-  NRF_SAADC->RESOLUTION = SAADC_RESOLUTION_VAL_12bit;
-  
-  NRF_SAADC->CH[0].CONFIG = 
-    (SAADC_CH_CONFIG_GAIN_Gain1_4 << SAADC_CH_CONFIG_GAIN_Pos) |
-    (SAADC_CH_CONFIG_MODE_SE << SAADC_CH_CONFIG_MODE_Pos) |
-    (SAADC_CH_CONFIG_REFSEL_Internal << SAADC_CH_CONFIG_REFSEL_Pos);
-  
-  NRF_SAADC->CH[0].PSELP = SAADC_CH_PSELP_PSELP_VDDHDIV5;
-  NRF_SAADC->CH[0].PSELN = SAADC_CH_PSELN_PSELN_NC;
-  
-  // Sample
-  NRF_SAADC->RESULT.PTR = (uint32_t)&raw_value;
-  NRF_SAADC->RESULT.MAXCNT = 1;
-  NRF_SAADC->TASKS_START = 1;
-  while (!NRF_SAADC->EVENTS_STARTED);
-  NRF_SAADC->EVENTS_STARTED = 0;
-  NRF_SAADC->TASKS_SAMPLE = 1;
-  while (!NRF_SAADC->EVENTS_END);
-  NRF_SAADC->EVENTS_END = 0;
-  NRF_SAADC->TASKS_STOP = 1;
-  while (!NRF_SAADC->EVENTS_STOPPED);
-  NRF_SAADC->EVENTS_STOPPED = 0;
-  NRF_SAADC->ENABLE = 0;
-
-  // Force explicit double-precision calculations
-  double raw_double = (double)raw_value;
-  double step1 = raw_double * 2.4;
-  double step2 = step1 / 4095.0;
-  double vddh = 5.0 * step2;
-
-  return (float)vddh;
-}
-
+#include "battery.h"
 
 static const struct device *const scd_sensor = DEVICE_DT_GET(DT_ALIAS(scd41));
 static const struct device *const bme_sensor = DEVICE_DT_GET(DT_ALIAS(bme280));
